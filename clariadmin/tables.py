e75f166a33846dee6651697c0b9c456c7ccaddb0
Laurent Coustet <ed@zehome.com>
2010-01-16 00:13:50 +0000

    Panneau admin / Modification en liste de machine clariadmin

--- clariadmin/tables.py
+++ clariadmin/tables.py
@@ -3,7 +3,8 @@
 import django_tables as tables
 from claritick.clariadmin.models import Host
 
-class DefaultHostTable(tables.ModelTable):  
+class DefaultHostTable(tables.ModelTable):
+    id = tables.Column('Id', visible=False)
     hostname = tables.Column('Hostname')
     site = tables.Column('Site')
     ip = tables.Column('IP(s)')
