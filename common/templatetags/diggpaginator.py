cb1e38789ae2311f7861fe4d02fd9ecb03df323b
Laurent Coustet <laurent.coustet@clarisys.fr>
2010-06-02 09:30:46 +0000

    Gestion du request.GET dans les pagination

--- common/templatetags/diggpaginator.py
+++ common/templatetags/diggpaginator.py
@@ -9,23 +9,27 @@ class DiggPaginatorNode(template.Node):
         self.page = template.Variable(page)
     
     def render(self, context):
+        query_dict = context["request"].GET.copy()
+        if "page" in query_dict:
+            del query_dict["page"]
         context.push()
+        context["query_string"] = query_dict.urlencode()
         context["page"] = self.page.resolve(context)
         t = template.Template("""
 <ol id="pagination-digg">
     {% if page.has_previous %}
-        <li class="previous"><a href="?page={{ page.previous_page_number }}">«Précédent</a></li>
+        <li class="previous"><a href="?{% if query_string %}{{ query_string|escape }}&{% endif %}page={{ page.previous_page_number }}">«Précédent</a></li>
     {% endif %}
     {% for num in page.page_range %}
         {% if not num %}<li><a href="#">...</a></li>
         {% else %}
             {% ifequal page.number num %}<li class="active">{{ num }}</li>
-            {% else %}<li><a href="?page={{ num }}">{{ num }}</a></li>
+            {% else %}<li><a href="?{% if query_string %}{{ query_string|escape }}&{% endif %}page={{ num }}">{{ num }}</a></li>
             {% endifequal %}
         {% endif %}
     {% endfor %}
     {% if page.has_next %}
-        <li class="next"><a href="?page={{ page.next_page_number }}">Suivant»</a></li>
+        <li class="next"><a href="?{% if query_string %}{{ query_string|escape }}&{% endif %}page={{ page.next_page_number }}">Suivant»</a></li>
     {% endif %}
 </ol>
         """)
