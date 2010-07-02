031e286379a1462b8eefab6f9d198a64e6e4bf3b
Philippe Pepiot <philippe.pepiot@clarisys.fr>
2010-07-02 13:56:38 +0000

    Changement d'utilisateur : code moins obscur + fixes

--- chuser/forms.py
+++ chuser/forms.py
@@ -4,21 +4,7 @@ from django import forms
 from dojango import forms as df
 from common.widgets import FilteringSelect
 from common.models import ClaritickUser
-from django.contrib.auth import SESSION_KEY
 
 
 class ChuserForm(forms.Form):
     user = df.ModelChoiceField(widget=FilteringSelect(), queryset=ClaritickUser.objects.all())
-    def __init__(self, *a, **kw):
-        request = kw.pop('request', None)
-        user = kw.pop('user', None)
-        super(ChuserForm, self).__init__(*a, **kw)
-
-        if request:
-            self.request = request
-
-    def save(self):
-        user = self.cleaned_data['user']
-        self.request.session[SESSION_KEY] = user.id
-        self.request.user = user
-
--- chuser/views.py
+++ chuser/views.py
@@ -3,7 +3,7 @@
 from django.http import HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
 from django.views.decorators.http import require_POST
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import SESSION_KEY
 from django.contrib.auth.decorators import login_required
 from forms import ChuserForm
 
@@ -14,10 +14,12 @@ def change_user(request):
     if not (request.user.is_superuser or request.session.get('was_superuser', False)):
         raise PermissionDenied
 
-    form = ChuserForm(request.POST, request=request)
+    form = ChuserForm(request.POST)
 
     if form.is_valid():
-        form.save()
+        user = form.cleaned_data['user']
+        request.session[SESSION_KEY] = user.id
+        request.user = user
         request.session['was_superuser'] = True
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
