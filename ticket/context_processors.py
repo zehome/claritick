f19be00685df9f0a87ecace97dedb3232232853b
Philippe Pepiot <philippe.pepiot@clarisys.fr>
2010-07-07 08:25:19 +0000

    Optimisation SQL pour index.html

--- ticket/context_processors.py
+++ ticket/context_processors.py
@@ -6,28 +6,30 @@ import settings
 SUMMARY_TICKETS=15
 
 def get_critical_tickets(request):
-    qs = Ticket.open_tickets.all()
-    qs = qs.filter_ticket_by_user(request.user).filter(priority__gt=3).order_by('-date_open')
+    qs = Ticket.open_tickets.select_related().only('id', 'title').\
+            filter_ticket_by_user(request.user).\
+            filter(priority__gt=3).\
+            order_by('-date_open')[:SUMMARY_TICKETS]
     return qs[:SUMMARY_TICKETS]
 
 def get_ticket_text_statistics(request):
     statList = []
     statList.append(u"Tickets sans client: %s" % (Ticket.objects.filter(client__isnull = True).count()),)
-    qs = Ticket.objects.all()
-    qs = qs.filter_ticket_by_user(request.user)
+    qs = Ticket.objects.select_related().only('id', 'date_open').\
+            filter_ticket_by_user(request.user)
     if qs:
         qss = qsstats.QuerySetStats(qs, 'date_open')
         statList.append(u"Ouverts aujourd'hui: %s" % (qss.this_day(),))
         statList.append(u"Ouverts ce mois: %s" % (qss.this_month(),))
         statList.append(u"Ouverts en %s: %s" % (datetime.date.today().year, qss.this_year(),))
-    
     return statList
 
 def get_ticket_alarm(request):
-    alarms = TicketAlarm.opened.all()
+    alarms = TicketAlarm.opened.select_related().only('id')
     qs = Ticket.objects.all().\
             filter_ticket_by_user(request.user).\
-            filter(ticketalarm__in=alarms)[:SUMMARY_TICKETS]
+            filter(ticketalarm__in=alarms).\
+            select_related().only('title', 'id')[:SUMMARY_TICKETS]
     return qs
 
 def ticket_views(request):
