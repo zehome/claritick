9182c0e74c556f25bd45f81ad8e8b57d3be98555
Philippe Pepiot <philippe.pepiot@clarisys.fr>
2010-07-16 14:11:04 +0000

    Email: message d'érreur + oublis

--- ticket/management/commands/email2ticket.py
+++ ticket/management/commands/email2ticket.py
@@ -10,24 +10,24 @@ from ticket.views import post_comment
 from common.html2text import html2text
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 def email2ticket(string):
-    """ Parse un mail et renvoie le ticket crée (ou modifié) """
+    """ Parse un mail et crée ou modifie le ticket concerné """
 
     ticket = Ticket()
 
     cur = mail = email.message_from_string(string)
 
-    references = cur.get('In-Reply-To', '')
+    references = cur.get('References', '').split()
+
+    if not references:
+        references = cur.get('In-Reply-To', '').split()
 
     if references:
-        ticket = Ticket.objects.get(message_id=references)
+        ticket = Ticket.objects.get(message_id__in=references)
     else:
         ticket = Ticket()
-
-    import pdb
-    pdb.set_trace()
-
 
     # Get first part
     while cur.is_multipart():
@@ -62,7 +62,7 @@ def email2ticket(string):
     # TODO à reflechir
     user = User.objects.get(username='admin')
 
-    if references:
+    if references: # New comment
         form = type("", (), {})()
         form.cleaned_data = {'comment': content, 'internal': False }
         form.instance = ticket
@@ -70,18 +70,33 @@ def email2ticket(string):
         request.user = user
         post_comment(form, request)
     else: # New ticket
-        ticket.text = content
-        pattern = re.compile('(\d+) (.*)')
-        m = pattern.search(cur.get('Subject', None))
-        m = m.groups()
-        ticket.client = Client.objects.get(pk=m[0])
-        ticket.title = " ".join([part[0] for part in email.header.decode_header(m[1])])
-        ticket.message_id = cur.get('Message-ID', None)
-        ticket.opened_by = user
-        ticket.state = State.objects.get(pk=settings.TICKET_STATE_NEW)
-        ticket.category = Category.objects.get(label='Ticket')
-        ticket.save()
-    return ticket
+        try:
+            ticket.text = content
+            pattern = re.compile('(\d+) (.*)')
+            m = pattern.search(cur.get('Subject', None))
+            m = m.groups()
+            ticket.client = Client.objects.get(pk=m[0])
+            ticket.title = " ".join([part[0] for part in email.header.decode_header(m[1])])
+            ticket.message_id = cur.get('Message-ID', None)
+            ticket.opened_by = user
+            ticket.state = State.objects.get(pk=settings.TICKET_STATE_NEW)
+            ticket.category = Category.objects.get(label='Ticket')
+            ticket.save()
+        except:
+            to = cur.get('Return-Path')
+            if to:
+                send_mail("Ticket invalide",
+                        "Votre ticket est invalide, veuillez recommencer s'il vous plait\n"
+                        "Rappels:\n"
+                        "- Le mail doit avoir pour sujet votre numéro client suivi par le titre du ticket\n"
+                        "- Le contenu du mail sera le contenu du ticket, il ne doit pas être vide\n"
+                        "- Pour des raisons de compatibilité nous vous conseillons d'utiliser un programme de messagerie tel que Thunderbird : http://fr.www.mozillamessaging.com/fr/\n"
+                        "--\n"
+                        "CLARISYS Informatique       http://www.clarisys.fr/\n"
+                        "1, Impasse de ratalens      31240 SAINT JEAN\n"
+                        "Téléphone: 09 72 11 43 60   Fax: 05 11 11 11 11\n"
+                        "\nEmail généré automatiquement par le système de suivi CLARITICK.",
+                        settings.DEFAULT_FROM_EMAIL, [to])
 
 class Command(BaseCommand):
 
@@ -89,12 +104,10 @@ class Command(BaseCommand):
         srv = imaplib.IMAP4_SSL(settings.IMAP_SERVER)
         srv.login(settings.IMAP_LOGIN, settings.IMAP_PASSWORD)
         srv.select('INBOX')
-        #typ, data = srv.search(None, 'UNSEEN')
-        typ, data = srv.search(None, 'ALL')
-        n = data[0].split()[-1]
-        if n:
-        #for n in data[0].split():
+        typ, data = srv.search(None, 'UNSEEN')
+        for n in data[0].split():
             typ, content = srv.fetch(n, '(RFC822)')
             content = content[0][1]
-            ticket = email2ticket(content)
-            print "NEW TICKET", ticket.id
+            email2ticket(content)
+
+
