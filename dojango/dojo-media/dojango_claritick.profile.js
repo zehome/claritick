c66f47235bb74b65e54419664f8d992d30607058
Laurent Coustet <laurent.coustet@clarisys.fr>
2010-07-27 15:29:58 +0000

    Ajout profil dojo

---
+++ dojango/dojo-media/dojango_claritick.profile.js
@@ -0,0 +1,40 @@
+dependencies ={
+
+    stripConsole: "normal",
+
+   layers:  [
+       {
+       name: "dojo.js",
+       dependencies: [
+           "dijit.dijit",
+           "dijit.Menu",
+           "dijit.Tree",
+           "dijit.Tooltip",
+           "dijit.Dialog",
+           "dijit.TooltipDialog",
+           "dijit.form.Button",
+           "dijit.form.DropDownButton",
+           "dijit.form.NumberSpinner",
+           "dijit.form.NumberTextBox",
+           "dijit.form.Textarea",
+           "dijit.form.DateTextBox",
+           "dijit.form.TimeTextBox",
+           "dijit.form.ValidationTextBox",
+           "dijit.form.ComboBox",
+           "dijit.form.Slider",
+           "dijit.form.Checkbox",
+           "dijit.form.FilteringSelect",
+           "dijit.layout.ContentPane",
+           "dijit.layout.TabContainer",
+           "dojo.dnd.Source",
+           "dojo.nls.dojo_fr-fr"
+       ]
+       }
+   ],
+
+   prefixes: [
+       [ "dijit", "../dijit" ],
+       [ "dojox", "../dojox" ],
+   ]
+
+ };
