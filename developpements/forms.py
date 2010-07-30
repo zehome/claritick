6cc908534ec238b3238fbd19d2a471ed41a2df01
Philippe Pepiot <philippe.pepiot@clarisys.fr>
2010-07-30 15:07:39 +0000

    SuiviDev: Préselection de ticket marche

--- developpements/forms.py
+++ developpements/forms.py
@@ -4,6 +4,7 @@ from django.db import models
 from dojango import forms as df
 
 from ticket.models import Ticket
+from common.models import Client as ClaritickClient
 from developpements.models import Developpement, GroupeDev, Project, Version
 
 class DeveloppementForm(df.ModelForm):
@@ -18,17 +19,19 @@ class DeveloppementForm(df.ModelForm):
         instance = kw.get("instance", None)
 
         if instance and instance.pk:
-            # TODO ça matchera jamais
-            # Il faut choper tous les fils d'un groupe
-            # de clients claritick...
-            tickets = Ticket.minimal.\
-                    filter(client__in=instance.client_demandeur.\
-                    filter(client__isnull=False))
+            # On cherche les tickets des clients claritick
+            clients_pk = []
+            for c in instance.client_demandeur.filter(client__isnull=False):
+                clients_pk.extend([x.pk for x in ClaritickClient.objects.\
+                        get_childs("parent", c.client.pk)])
+
+            tickets = Ticket.minimal.filter(client__in=clients_pk)
 
             self.base_fields["groupe"].queryset = GroupeDev.objects.\
                     filter(project=instance.groupe.project)
             self.base_fields["version_requise"].queryset = Version.objects.\
                     filter(project=instance.groupe.project)
+
         else:
             tickets = Ticket.minimal.none()
 
@@ -40,7 +43,6 @@ class DeveloppementForm(df.ModelForm):
             projet_field.choices.insert(0, ("",""))
 
             self.base_fields.insert(0, "projet", projet_field)
-
 
         self.base_fields["ticket"].queryset = tickets
 
