240645d685f526fe87145a4bd3b14d12a15fc57e
Laurent Coustet <laurent.coustet@clarisys.fr>
2010-10-06 15:24:24 +0000

    Probleme de droits

--- reporting/views.py
+++ reporting/views.py
@@ -6,14 +6,14 @@ from django.contrib.auth.decorators import login_required
 import reporting
 
 @login_required
-@permission_required("tickets.can_view_report")
+@permission_required("ticket.can_view_report")
 def report_list(request):
     reports = reporting.all_reports()
     return render_to_response('reporting/list.html', {'reports': reports}, 
                               context_instance=RequestContext(request))
 
 @login_required
-@permission_required("tickets.can_view_report")
+@permission_required("ticket.can_view_report")
 def view_report(request, slug):
     report = reporting.get_report(slug)(request)
     data = {'report': report, 'title':report.verbose_name}
