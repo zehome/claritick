90b7736a33aefee86ef58052344e01cce09f2df2
christophe31 <christophe31>
2011-02-16 09:06:43 +0000

    optimise a bit clariadmin.js && clarify future usage of js fun typeChanged

--- media/js/clariadmin.js
+++ media/js/clariadmin.js
@@ -15,14 +15,10 @@ function confirmDialog(title, question, callbackFn, e) {
         { label: 'Oui', id: 'yes', onClick: callback });
     var noButton = new dijit.form.Button(
         { label: 'Non', id: 'no', onClick: callback });
-
     errorDialog.containerNode.appendChild(questionDiv);
     errorDialog.containerNode.appendChild(yesButton.domNode);
     errorDialog.containerNode.appendChild(noButton.domNode);
     errorDialog.show();
-}
-function is_string(a) {
-    return typeof a == 'string';
 }
 function postwith (inputs,post_url_or_form) {
     function convert_to_form(url){
@@ -30,7 +26,7 @@ function postwith (inputs,post_url_or_form) {
         f.method="POST" ;f.action = url;
         return f;
     }
-    f = is_string(post_url_or_form)?convert_to_form(post_url_or_form):post_url_or_form;
+    f = (typeof post_url_or_form == 'string')?convert_to_form(post_url_or_form):post_url_or_form;
     inputs["csrfmiddlewaretoken"]=document.getElementsByName("csrfmiddlewaretoken")[0].value;
     for (var k in inputs){
         var i = document.createElement("input") ;
