941d4c1504ed6339165bfac453f4744c28b59f3e
Laurent Coustet <ed@zehome.com>
2011-02-22 23:25:04 +0000

    Ajout du userlevel dans le middleware ticketing system (rien  a faire la mais bon..)

--- ticket/middleware.py
+++ ticket/middleware.py
@@ -6,13 +6,15 @@ from common.exceptions import NoProfileException
 class PopulateUserMiddleware(object):
     def process_request(self, request):
         if hasattr(request, 'user') and not request.user.is_anonymous():
-            if request.user.is_superuser:
-                request.user.clients = Client.objects.all()
-                request.user.childs = ClaritickUser.objects.all()
-            else:
-                try:
-                    request.user.clients = request.user.get_profile().get_clients()
+            try:
+                profile = request.user.get_profile()
+                if request.user.is_superuser:
+                    request.user.clients = Client.objects.all()
+                    request.user.childs = ClaritickUser.objects.all()
+                else:
+                    request.user.clients = profile.get_clients()
                     request.user.childs = ClaritickUser.objects.get(pk=request.user.pk).get_child_users()
-                except UserProfile.DoesNotExist:
-                    raise NoProfileException(request.user)
+                request.user.security_level = profile.get_security_level()
+            except UserProfile.DoesNotExist:
+                raise NoProfileException(request.user)
         return None
