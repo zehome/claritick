cda1f8062c3edfc13d9633b4ce17ab8c800724d8
Laurent Coustet <ed@zehome.com>
2011-02-22 20:03:13 +0000

    Oops, needs USER=

--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ DATABASES = {
     "default": {
         "ENGINE": 'django.db.backends.postgresql_psycopg2',
         "NAME": 'claritick',
+        "USER": 'claritick',
         "PASSWORD": '',
         "HOST": "localhost",
         "PORT": "5432"
