75c1cc962c05527561252ce60ffdbd4a03171f6b
christophe31 <christophe31>
2011-02-24 09:09:28 +0000

    add a modification link and put the default host type in the popup for new param_additionnal_fields from host type.

--- media/admin_hosttype/admin_hosttype.js
+++ media/admin_hosttype/admin_hosttype.js
@@ -1,5 +1,15 @@
 dojo.addOnLoad(function(){
-    dojo.query('.module table>thead>tr>th')[0].attributes.removeNamedItem('colspan');
-    //dojo.query('.module .add-row>td')[0].innerHTML="<a id='id='add_id_truc' class='add-another' onclick='return showAddAnotherPopup(this);' href='/admin/clariadmin/paramadditionnalfield/add/'>Ajouter un objet Définition De Champs Complémentaires supplémentaire</a>";
+    dojo.query('#paramadditionnalfield_set-group .module table>thead>tr>th')[0].attributes.removeNamedItem('colspan');
+    list = dojo.query('.module table>tbody>tr .data_type');
+    for( i in list)
+    {
+        if(! isNaN(parseInt(i))){
+            list[i].innerHTML="<a href='/admin/clariadmin/paramadditionnalfield/"+list[i].parentNode.children[0].children[1].value+"'>"+list[i].innerHTML+"</a>";
+        }
+    }
+    dojo.connect(dojo.byId("#paramadditionnalfield_set-group"),"onmouseover",function(){
+            dojo.query('#paramadditionnalfield_set-group .module .add-row>td')[0].innerHTML = "<a id='id='add_id_truc' class='add-another' onclick='return showAddAnotherPopup(this);' href='/admin/clariadmin/paramadditionnalfield/add/?host_type="+dojo.byId('id_paramadditionnalfield_set-__prefix__-host_type').value+"'>Ajouter un objet Définition De Champs Complémentaires supplémentaire</a>";}
+        )
+
 });
 
