b680e360bdff8c88b477520467ff868eeebb777c
christophe31 <christophe31>
2011-02-25 08:31:23 +0000

    optimise dojo.query requests

--- media/admin_hosttype/admin_hosttype.js
+++ media/admin_hosttype/admin_hosttype.js
@@ -1,22 +1,22 @@
 var add_row = null;
 dojo.addOnLoad(function(){
-    dojo.query('#paramadditionnalfield_set-group .module table>thead>tr>th')[0].attributes.removeNamedItem('colspan');
-    list = dojo.query('#paramadditionnalfield_set-group .module table>tbody>tr .data_type');
+    dojo.query('#paramadditionnalfield_set-group th')[0].attributes.removeNamedItem('colspan');
+    list = dojo.query('#paramadditionnalfield_set-group .data_type');
     for( i in list)
     {
         if(! isNaN(parseInt(i))){
-            list[i].innerHTML="<a href='javascript:void(0)' onclick='return showPopup(\"/admin/clariadmin/paramadditionnalfield/"+list[i].parentNode.children[0].children[1].value+"\")'>"+list[i].innerHTML+"</a>";
+            list[i].innerHTML="<a id='edit_field_"+i+"' href='javascript:void(0)' onclick='return showPopup(\"/admin/clariadmin/paramadditionnalfield/"+list[i].parentNode.children[0].children[1].value+"\")'>"+list[i].innerHTML+"</a>";
         }
     }
     dojo.connect(dojo.byId("#paramadditionnalfield_set-group"),"onmouseover",function(){
         if (add_row == null){
-            add_row = dojo.query('#paramadditionnalfield_set-group .module .add-row>td')[0]
+            add_row = dojo.query('#paramadditionnalfield_set-group .add-row>td')[0]
             add_row.style.textAlign="center";
             normal_text = "<span><img src='/adm_media/img/admin/icon_addlink.gif' alt='Add'>Cliquez ici pour ajouter une entrée.</span>";
             over_text = "<span>﻿</span>"
             add_row.innerHTML = normal_text ;
             dojo.connect(add_row, "onclick",function(){
-                addParameterField("/admin/clariadmin/paramadditionnalfield/add/?host_type="+dojo.byId('id_paramadditionnalfield_set-__prefix__-host_type').value);});
+                showPopup("/admin/clariadmin/paramadditionnalfield/add/?host_type="+dojo.byId('id_paramadditionnalfield_set-__prefix__-host_type').value);});
             dojo.connect(add_row, "onmouseover",function(){
                 add_row.innerHTML = over_text;
                 add_row.style.background="#E1FFE1 url(/adm_media/img/admin/icon_addlink.gif) center center no-repeat";
@@ -27,11 +27,9 @@ dojo.addOnLoad(function(){
             });
         }
     })
-
-
 });
 function showPopup(url){
-        var win = window.open( url , name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+        var win = window.open( url , name, 'height=500,width=850,resizable=yes,scrollbars=yes');
         win.focus();
         return false;
 }
