a30b0b8ab748009b00d1141a4ca9098778c3b382
christophe31 <christophe31>
2011-03-07 08:58:36 +0000

    fix pages and searches + cleanup

--- clariadmin/views.py
+++ clariadmin/views.py
@@ -48,7 +48,7 @@ def global_search(user, search, qs):
            'type':'text'}
     # Filtre les foreign key en fonction du niveau de securite.
     authorized_keys = SearchHostForm.filter_list(user,fks.keys())
-    fks = dict([(k,v) for k,v in fks.iteritems() 
+    fks = dict([(k,v) for k,v in fks.iteritems()
         if k in authorized_keys])
 
     # Filter local fields
@@ -64,7 +64,7 @@ def global_search(user, search, qs):
                      for key in fields if key not in fks.keys()))
         ) | (
             # Do search on filtered foreign keys
-            reduce(ior,(Q(**{"%s__%s__icontains" % (key, value): search}) 
+            reduce(ior,(Q(**{"%s__%s__icontains" % (key, value): search})
                      for key, value in fks.iteritems()))
         )
     )
@@ -93,7 +93,7 @@ def list_all(request, *args, **kw):
     form_extra = False
     sort_default = "-id"
     columns = HostForm.filter_list(request.user, (
-		"hostname", "ip", "site", "type", "os", "model", "status"))
+        "hostname", "ip", "site", "type", "os", "model", "status"))
 
     # Récupère le type d'hote pour adapter si besoin l'AdditionnalFieldForm.
     host_type = request.session.get('search_host_form_fields', {}).get('type', False)
@@ -107,12 +107,12 @@ def list_all(request, *args, **kw):
                 # récupère les éléments de POST propre à SearchHostForm
                 post_filtred = dict((k, v) for k, v in POST.iteritems()
                                     if k in form.cleaned_data.keys())
-    
+
                 # si recherche != dernière recherche, retour page 1 et update session
                 if request.session.get('search_host_form_fields', {}) != post_filtred:
                     new_search = True
                     request.session['search_host_form_fields'] = post_filtred
-    
+
                 if host_type:
                     form_extra = AdditionnalFieldForm.get_form(POST, host_type=HostType.objects.get(pk=host_type))
                     request.session['additionnal_field_form_fields'] = dict(
@@ -129,16 +129,14 @@ def list_all(request, *args, **kw):
     form.fields = SearchHostForm.filter_querydict(request.user, form.fields)
 
     #get session/GET parametters
-    sorting = request.GET.get('sort', request.session.get("sort_adm_list", sort_default))
-
-    if sorting not in columns:
-        sorting = sort_default
-    if sorting.startswith('-') and sorting[1:] not in columns:
-        sorting = sort_default
-    if request.GET.get('sort', False):
-        request.session["sort_adm_list"] = sorting
-    if request.GET.get('page', False):
-        request.session["page_adm_list"] = request.GET.get('page', False)
+    sorting = sort_default
+    sort_get = request.GET.get('sort',
+                               request.session.get("sort_adm_list", sort_default))
+    if sort_get in columns:
+        sorting = sort_get
+    if sort_get.startswith('-') and sort_get[1:] in columns:
+        sorting = sort_get
+    request.session["sort_adm_list"] = sorting
 
     qs = Host.objects.none()
     #apply searchs if any.
@@ -147,7 +145,7 @@ def list_all(request, *args, **kw):
         if search or [e for e in form.cleaned_data.values() if e]:
             qs = Host.objects.filter_by_user(request.user)
             if search:
-                qs = global_search(request.user,search, qs)
+                qs = global_search(request.user, search, qs)
             if form_extra:
                 qs = filter_hosts(qs, sorting, form.cleaned_data,
                      form_extra.get_data())
@@ -159,9 +157,11 @@ def list_all(request, *args, **kw):
     paginator = DiggPaginator(qs, settings.TICKETS_PER_PAGE, body=5, tail=2, padding=2)
 
     page_num = 1
-    page_asked = int(request.session.get('page_adm_list', 1))
-    if (page_asked <= paginator.num_pages):
+    page_asked = int(request.session.get('page_adm_list',
+                                         request.GET.get('page', 1)))
+    if ((page_asked <= paginator.num_pages) and not new_search):
         page_num = page_asked
+    request.session["page_adm_list"] = page_num
     page = paginator.page(page_num)
 
     return render_to_response("clariadmin/list.html", {
@@ -178,7 +178,7 @@ def new(request, from_host=False):
     View to Create a new host. (eventualy copied from an existing one)
     """
     POST = HostForm.filter_querydict(request.user, request.POST)
-    add_fields=None
+    add_fields = None
 
     if POST:
         form = HostForm(request.user, POST)
@@ -203,7 +203,8 @@ def new(request, from_host=False):
             form = HostForm(request.user)
     return render_to_response('clariadmin/host.html', {
             'form': form,
-            'additionnal_fields':add_fields }, context_instance=RequestContext(request))
+            'additionnal_fields': add_fields},
+            context_instance=RequestContext(request))
 
 @permission_required("clariadmin.can_access_clariadmin")
 def modify(request, host_id):
@@ -236,7 +237,7 @@ def modify(request, host_id):
 
     return render_to_response("clariadmin/host.html", {
         "form": form,
-        'additionnal_fields':add_fields,
+        'additionnal_fields': add_fields,
         "host": host}, context_instance=RequestContext(request))
 
 @permission_required("clariadmin.can_access_clariadmin")
