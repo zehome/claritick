4ace7255a4d065c1f111a06bc7ec942d691d1b45
christophe31 <christophe31>
2011-03-07 13:07:19 +0000

    cleanup javascript

--- media/js/clariadmin.js
+++ media/js/clariadmin.js
@@ -12,9 +12,9 @@ function confirmDialog(title, question, callbackFn, e) {
     };
     var questionDiv = dojo.create('div', { innerHTML: question });
     var yesButton = new dijit.form.Button(
-        { label: 'Oui', id: 'yes', onClick: callback });
+        {label: 'Oui', id: 'yes', onClick: callback});
     var noButton = new dijit.form.Button(
-        { label: 'Non', id: 'no', onClick: callback });
+        {label: 'Non', id: 'no', onClick: callback});
     errorDialog.containerNode.appendChild(questionDiv);
     errorDialog.containerNode.appendChild(yesButton.domNode);
     errorDialog.containerNode.appendChild(noButton.domNode);
@@ -22,19 +22,20 @@ function confirmDialog(title, question, callbackFn, e) {
 }
 function postwith (inputs,post_url_or_form) {
     function convert_to_form(url){
-        f=document.createElement("form");
-        f.method="POST" ;f.action = url;
+        f = document.createElement("form");
+        f.method="POST" ;
+        f.action = url;
         return f;
     }
     f = (typeof post_url_or_form == 'string')?convert_to_form(post_url_or_form):post_url_or_form;
-    inputs["csrfmiddlewaretoken"]=document.getElementsByName("csrfmiddlewaretoken")[0].value;
+    inputs["csrfmiddlewaretoken"] = document.getElementsByName("csrfmiddlewaretoken")[0].value;
     for (var k in inputs){
         var i = document.createElement("input") ;
         i.setAttribute("name", k) ;
         i.setAttribute("value", inputs[k]);
         f.appendChild(i) ;
     }
-    document.body.appendChild(f) ;
+    document.body.appendChild(f);
     f.submit() ;
-    document.body.removeChild(f) ;
+    document.body.removeChild(f);
 }
