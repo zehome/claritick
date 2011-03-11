62aa8ee7f1570853d2111f6eada2d82c4da118a7
Laurent Coustet <laurent.coustet@clarisys.fr>
2011-03-11 15:44:31 +0000

    Cleanup

--- clariadmin/urls.py
+++ clariadmin/urls.py
@@ -3,10 +3,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('clariadmin.views',
-    url(r'^$',          'list_all', name='list_hosts'),
-    url(r'^new/$',                 'new', name='new_host'),
+    url(r'^$', 'list_all', name='list_hosts'),
+    url(r'^new/$', 'new', name='new_host'),
     url(r'^new/from/(-?\d+)$', 'new'),
-    url(r'^modify/(-?\d+)$',          'modify'),
-    url(r'^list/(all)?$',          'list_all', name='list_hosts'),
+    url(r'^modify/(-?\d+)$', 'modify'),
+    url(r'^list/(all)?$', 'list_all', name='list_hosts'),
     url(r'^getExtrafieldsForm/(-?\d+)(/b)?', 'ajax_extra_fields_form'),
 )
