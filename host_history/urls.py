d21e054fe5b0bed170e5c111a02ef19fc5af41d3
christophe31 <christophe31>
2011-04-04 15:38:08 +0000

    host_history:<urls> map ^list/$ on 'list_logs' view

--- host_history/urls.py
+++ host_history/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('host_history.views',
        url(r'^$', 'list_logs', name='list_logs'),
+       url(r'^list/$', 'list_logs', name='list_logs'),
        url(r'^list/(host)/(\S*)$', 'list_logs', name='list_logs'),
        url(r'^list/(user)/(\S*)$', 'list_logs', name='list_logs'),
        url(r'^show_diff/(\d+)','view_changes',name='view_changes'),
