d12ef11f7f0c35227fb7cc6176ed1b4fee76b9a1
Laurent Coustet <laurent.coustet@clarisys.fr>
2011-04-13 17:00:47 +0000

    Don't care session with this module

--- host_history/views.py
+++ host_history/views.py
@@ -22,24 +22,8 @@ def list_logs(request, filter_type=None, filter_key=None):
         - sort_log_list: garde l'ordre courrent
     """
     sort_default='-date'
-    columns = ["host","user","date","ip","action","message","version"]
+    columns = ["host", "user", "date", "ip", "action", "message", "version"]
     new_search = False
-
-    if request.GET.get("reset", "0") == "1":
-        try:
-            del request.session["lastpage_log_list"] 
-        except KeyError:
-            pass
-        try:
-            del request.session["search_log_list"] 
-        except KeyError:
-            pass
-        try:
-            del request.session["sort_log_list"] 
-        except KeyError:
-            pass
-        return redirect('list_logs')
-
 
     # Url filtering
     if filter_type:
@@ -51,31 +35,24 @@ def list_logs(request, filter_type=None, filter_key=None):
             raise Http404
     else:
         qs = HostEditLog.objects.all()
-    qs = qs.select_related('host','hostrevision')
+    qs = qs.select_related('host', 'hostrevision')
 
     # Reset button
-    if request.POST.get("filter_reset",False):
-        form = SearchLogForm(request.user,{})
-        del(request.session["lastpage_log_list"])
-        del(request.session["search_log_list"])
-        del(request.session["sort_log_list"])
+    if request.POST.get("filter_reset", False):
+        form = SearchLogForm(request.user, {})
 
     # Handle SearchForm filtering
-    form = SearchLogForm(request.user, request.POST or
-                                     request.session.get("search_log_list",{}))
+    form = SearchLogForm(request.user, request.POST)
     if form.is_valid():
-        request.session["search_log_list"] = form.cleaned_data
         qs = form.search(qs)
 
     # Update sorting
     sorting = sort_default
-    sort_get = request.GET.get('sort',
-                   request.session.get("sort_log_list", sort_default))
+    sort_get = request.GET.get('sort', sort_default)
     if sort_get in columns:
         sorting = sort_get
     if sort_get.startswith('-') and sort_get[1:] in columns:
         sorting = sort_get
-    request.session["sort_log_list"] = sorting
 
     # Set paginator
     paginator = DiggPaginator(qs.order_by(sorting),
@@ -83,11 +60,9 @@ def list_logs(request, filter_type=None, filter_key=None):
 
     # Get page
     page_num = 1
-    page_asked = int(request.GET.get('page',
-                                     request.session.get('lastpage_log_list', 1)))
+    page_asked = int(request.GET.get('page', 1))
     if ((page_asked <= paginator.num_pages) and not new_search):
         page_num = page_asked
-    request.session["lastpage_log_list"] = page_num
     page = paginator.page(page_num)
     return render_to_response(
         'host_history/list.html',
