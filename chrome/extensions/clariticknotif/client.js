cefd385f812556b20b521fce53b7da49d15aa279
Laurent Coustet <laurent.coustet@clarisys.fr>
2011-05-09 09:43:53 +0000

    Claritick notification extension

---
+++ chrome/extensions/clariticknotif/client.js
@@ -0,0 +1,161 @@
+var pending_notifications = [];
+var django_notifications_url = JSON.parse(localStorage.djangoURL);
+var django_notifications_get_url = django_notifications_url + 'get/';
+var django_notifications_check_url = django_notifications_url + 'is_authenticated/';
+var is_loggedin = false;
+var current_username = null;
+var n_socket;
+
+const CHECK_LOGIN_TIMEOUT = 60 * 1000; /* Every minute */
+
+/* This will create a timeouting notifications (in seconds) */
+function createNotification(image, title, content, timeout)
+{
+    var n = webkitNotifications.createNotification(image, title, content);
+    var notification_timeout_callback = function(notification) 
+    {
+        return function() 
+        {
+            notification.cancel();
+        };
+    };
+    setTimeout(notification_timeout_callback(n), timeout * 1000);
+    return n;
+}
+
+function createHTMLNotification(url, timeout)
+{
+    var n = webkitNotifications.createHTMLNotification(url);
+    var notification_timeout_callback = function(notification) 
+    {
+        return function() 
+        {
+            notification.cancel();
+        };
+    };
+    setTimeout(notification_timeout_callback(n), timeout * 1000);
+    return n;    
+}
+
+function show() 
+{
+    var notification = webkitNotifications.createHTMLNotification(django_notifications_get_url + '1');
+    notification.show();
+    var errorCallback = function (notification) 
+    {
+        return function(e) {
+            console.log("Error showing notification: " + e);
+            pending_notifications.pop(notification);
+        };
+    };
+    var onDisplayCallback = function (notification) 
+    {
+        return function() {
+            console.log("Shown notification: " + notification);
+            pending_notifications.pop(notification);
+        };
+    };
+    var onCloseCallback = function (notification) 
+    {
+        return function() {
+            console.log("Closed notification: " + notification);
+            pending_notifications.pop(notification);
+        };
+    };
+    notification.onerror = errorCallback(notification);
+    notification.ondisplay = onDisplayCallback(notification);
+    notification.onclose = onCloseCallback(notification);
+    pending_notifications.push(notification);
+}
+
+/* Check if the user is logged in */
+function check_auth(first_time)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", django_notifications_check_url, true);
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState == 4) 
+        {
+            // JSON.parse does not evaluate the attacker's scripts.
+            var resp = xhr.responseText;
+            if (resp != "0")
+            {
+                /* Login ok */
+                current_username = resp;
+                is_loggedin = true;
+                if (first_time)
+                {
+                    var notification = createNotification(null, "Sucessfully registered: "+current_username, current_username + ": logged in. You will receive notifications.", 3);
+                    setup_websocket();
+                    notification.show();
+                }
+            } else {
+                is_loggedin = false;
+                current_username = null;
+                var notification = createNotification(null, "Not logged in", "You are not loggedin. You will not receive notifications :(", 10);
+                notification.show();
+            }
+        }
+    }
+    xhr.send();
+}
+
+function setup_websocket()
+{
+    if (! is_loggedin || ! current_username)
+    {
+        console.error("Not possible to setup websocket: login required.");
+        return null;
+    }
+    var host = JSON.parse(localStorage.wsHost);
+    var port = JSON.parse(localStorage.wsPort);
+    var isSecure = JSON.parse(localStorage.isSecure);
+    
+    /* Initialize websocket */
+    n_socket = new io.Socket(host, {port: port, secure: isSecure});
+    n_socket.connect();
+    n_socket.on('connect', function(evt) 
+        {
+            console.log("Sending register.");
+            n_socket.send(JSON.stringify({action: "register", username: current_username}));
+        }
+    );
+    n_socket.on('message', function(evt) 
+        {
+            /* Ack ? */
+            if (! evt || evt == "ack")
+            {
+                console.log("ack received.");
+                return;
+            }
+            
+            console.log("Message received: "+evt);
+            try {
+                var data = JSON.parse(evt);
+            } catch (e) {
+                console.error("Unable to decode message");
+                return;
+            }
+            if (data.action && data.action == "notification")
+            {
+                var n = createHTMLNotification(django_notifications_get_url + data.id_notification);
+                n.show();
+            }
+        }
+    );
+    n_socket.on('error', function(evt)
+        {
+            console.error("Error: "+evt);
+        }
+    );
+}
+
+if (!localStorage.isInitialized)
+    resetSettings();
+
+if (JSON.parse(localStorage.isActivated))
+{
+    setInterval("check_auth()", CHECK_LOGIN_TIMEOUT);
+    check_auth(true); /* Check just now */
+}
---
+++ chrome/extensions/clariticknotif/options.js
@@ -0,0 +1,113 @@
+/* Switch widgets On or Off based on isActivated widget */
+function ghostOptions() 
+{
+    var isDeactivated = !options.isActivated.checked;
+    console.log("ghostOptions");
+    options.style.color = isDeactivated ? 'graytext' : 'black';
+    
+    for (var i=0; i < options_widgets.text.length; i++)
+    {
+        console.log("options.disabled: " + isDeactivated);
+        options[options_widgets.text[i].name].disabled = isDeactivated;
+    }
+    for (var i=0; i < options_widgets.checkbox.length; i++)
+        if (! options_widgets.checkbox[i].onchange_callback)
+            options[options_widgets.checkbox[i].name].disabled = isDeactivated;
+}
+
+var options_widgets = {
+    text: [
+        { name: "wsHost", default: "localhost" },
+        { name: "wsPort", default: 8010 },
+        { name: "djangoURL", default: "http://localhost:8000/notifications/" },
+        { name: "notificationTimeout", default: 10 }
+    ],
+    checkbox: [
+        { name: "isActivated", default: false, onchange_callback: ghostOptions },
+        { name: "isSecure", default: true },
+        { name: "onNewTicket", default: true }, 
+        { name: "onNewTicketAssignedTo", default: true },
+        { name: "onNewAnswer", default: true }
+    ]
+};
+
+function loadSettings()
+{
+    for (var i=0; i < options_widgets.text.length; i++)
+    {
+        var widgetName = options_widgets.text[i].name;
+        console.log('Setting value of ' + options[widgetName] + ' => ' +JSON.parse(localStorage[widgetName]));
+        options[widgetName].value = JSON.parse(localStorage[widgetName]);
+    }
+    for (var i=0; i < options_widgets.checkbox.length; i++) 
+    {
+        var widgetName = options_widgets.checkbox[i].name;
+        console.log('Setting value of ' + options[widgetName] + ' => ' +JSON.parse(localStorage[widgetName]));
+        options[widgetName].checked = JSON.parse(localStorage[widgetName]);
+    }
+}
+
+function resetSettings()
+{
+    for (var i=0; i < options_widgets.text.length; i++)
+    {
+        var widgetOption = options_widgets.text[i];
+        var default_value = widgetOption.default;
+        localStorage[widgetOption.name] = JSON.stringify(default_value);
+    }
+    for (var i=0; i < options_widgets.checkbox.length; i++) 
+    {
+        var widgetOption = options_widgets.checkbox[i];
+        var default_value = widgetOption.default;
+        localStorage[widgetOption.name] = default_value;
+    }
+    localStorage.isInitialized = true;
+}
+
+/* Listen events on option widgets, store the result
+ * in the local Storage
+ */
+function loadOptionPage()
+{
+    try {
+        loadSettings();
+    } catch (e) {
+        console.log("Unable to load settings. Reinitializing it.");
+        console.log("Original error: "+e);
+        localStorage.isInitialized = false;
+        resetSettings();
+        loadSettings();
+    }
+    
+    /* Load page colors */
+    ghostOptions();
+    
+    /* Connect widgets to localStorage */
+    for (var i=0; i < options_widgets.checkbox.length; i++) 
+    {
+        var widgetOption = options_widgets.checkbox[i];
+        var widget = options[widgetOption.name];
+        var onchange = function(widget, widgetOption) {
+            return function() {
+                localStorage[widgetOption.name] = options[widgetOption.name].checked;
+                if (widgetOption.onchange_callback) {
+                    widgetOption.onchange_callback();
+                }
+            }
+        };
+        widget.onchange = onchange(widget, widgetOption);
+    }
+    for (var i=0; i < options_widgets.text.length; i++) 
+    {
+        var widgetOption = options_widgets.text[i];
+        var widget = options[widgetOption.name];
+        var onchange = function(widget, widgetOption) {
+            return function() {
+                localStorage[widgetOption.name] = options[widgetOption.name].checked;
+                if (widgetOption.onchange_callback) {
+                    widgetOption.onchange_callback();
+                }
+            }
+        };
+    }
+}
