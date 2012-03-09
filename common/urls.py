ee306295d88b73ecf7c1bc1ed714d8f3ca73372c
Laurent Coustet <laurent.coustet@clarisys.fr>
2012-03-09 13:07:44 +0000

    Supprime le système "exportable" qui n'est pas fonctionnel

--- common/urls.py
+++ common/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('common.views',
     url(r'^client/$', 'infos_login', name="infos_login"),
     url(r'^client/modify/(?P<client_id>\d+)/$', 'modify_client', name="modify_client"),
     url(r'^trafiquable/$', 'trafiquable'),
-    url(r'^exportable/$', 'exportable'),
 )
