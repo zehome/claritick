e07e3725b389ac4cd1d969b188e453225efc90c4
Laurent Coustet <laurent.coustet@clarisys.fr>
2012-08-29 18:26:57 +0200

    pep8

--- claritick/ticket/context_processors.py
+++ claritick/ticket/context_processors.py
@@ -9,12 +9,12 @@ def get_active_alarms(request):
 def ticket_views(request):
     if request.user and not request.user.is_anonymous():
         return {
-        "ticket_views": TicketView.objects.filter(user=request.user),
-        "settings": {
-            "TICKET_STATE_CLOSED": settings.TICKET_STATE_CLOSED,
-            "TICKET_STATE_NEW": settings.TICKET_STATE_NEW,
-            "TICKET_STATE_ACTIVE": settings.TICKET_STATE_ACTIVE,
+            "ticket_views": TicketView.objects.filter(user=request.user),
+            "settings": {
+                "TICKET_STATE_CLOSED": settings.TICKET_STATE_CLOSED,
+                "TICKET_STATE_NEW": settings.TICKET_STATE_NEW,
+                "TICKET_STATE_ACTIVE": settings.TICKET_STATE_ACTIVE,
             },
-        "ticket_alarms_count": get_active_alarms(request),
-        }
+            "ticket_alarms_count": get_active_alarms(request),
+    }
     return {}
